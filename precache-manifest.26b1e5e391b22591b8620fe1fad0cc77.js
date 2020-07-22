92391128f6136fad95b72eb10d5af38cd88cafe0
DSP195 <sp.desaishyam@gmail.com>
2020-07-22 17:13:36 +0530

    Updates

---
+++ precache-manifest.26b1e5e391b22591b8620fe1fad0cc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e4e82354c185e5000da964061d2e42",
+    "url": "/React-Apps/pics/index.html"
+  },
+  {
+    "revision": "f5a591a47ded99ce93d8",
+    "url": "/React-Apps/pics/static/css/main.481a0009.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/React-Apps/pics/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Apps/pics/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a591a47ded99ce93d8",
+    "url": "/React-Apps/pics/static/js/main.30693750.chunk.js"
+  },
+  {
+    "revision": "c588ee746a424ef0db6b",
+    "url": "/React-Apps/pics/static/js/runtime-main.3e38cba4.js"
+  }
+]);
